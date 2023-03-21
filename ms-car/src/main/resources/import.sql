3e7e380bd423749d0aa27644c8c11309a189d9a1
brazil-bruno <bruno.brazil2022@gmail.com>
2023-03-21 09:06:06 -0300

    ms-car project created

---
+++ ms-car/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_car (car_Model, description, price) VALUES ('Renault Logan', '1.6 Engine, 4 doors', '189.0');
+INSERT INTO tb_car (car_Model, description, price) VALUES ('Fiat Toro', '1.8 Engine, 2 doors', '200.0');
